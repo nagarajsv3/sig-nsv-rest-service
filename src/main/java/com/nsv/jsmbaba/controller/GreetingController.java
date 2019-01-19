57734eeb36a7c1e19ccd5cddda3cabc6b6a78960
nagarajsv3 <nagarajsv3@gmail.com>
2019-01-19 12:39:33 -0500

    Rest Service

---
+++ src/main/java/com/nsv/jsmbaba/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.nsv.jsmbaba.controller;
+
+import com.nsv.jsmbaba.domain.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final AtomicLong atomicLong = new AtomicLong();
+    private static final String template = "Hello %s";
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name",defaultValue = "world") String name){
+        return new Greeting(atomicLong.getAndIncrement(),String.format(template,name));
+    }
+}
---
+++ src/main/java/com/nsv/jsmbaba/domain/Greeting.java
@@ -0,0 +1,15 @@
+package com.nsv.jsmbaba.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private long id;
+    private String name;
+}
